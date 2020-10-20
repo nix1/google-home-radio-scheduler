0151fcf1168534dcfe10ef3698dd4bb46c282a02
Paweł Sierszeń <paul@codility.com>
2020-10-20 19:42:08 +0200

    Extract casting a stream to a function

--- play.py
+++ play.py
@@ -1,36 +1,37 @@
-from pychromecast.discovery import discover_chromecasts, stop_discovery
-from pychromecast.controllers import BaseController
-from pychromecast import Chromecast
-import pychromecast
+from pychromecast import discovery, get_listed_chromecasts
 import time
 
-# config
-device_name = 'Pizza'
-rns = 'https://n10a-eu.rcs.revma.com/ypqt40u0x1zuv'
 
-# List chromecasts on the network, but don't connect
-services, browser = discover_chromecasts()
+def play_stream(device_name, url):
+    # List chromecasts on the network, but don't connect
+    services, browser = discovery.discover_chromecasts()
 
+    # Shut down discovery
+    discovery.stop_discovery(browser)
 
-# Shut down discovery
-stop_discovery(browser)
+    # Discover and connect to chromecasts
+    chromecasts, browser = get_listed_chromecasts(friendly_names=[device_name])
+    cast = chromecasts[0]
+    # Start worker thread and wait for cast device to be ready
+    cast.wait()
 
-# Discover and connect to chromecasts
-chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[device_name])
-cast = chromecasts[0]
-# Start worker thread and wait for cast device to be ready
-cast.wait()
+    mc = cast.media_controller
+    mc.play_media(url, 'audio/mpeg')
+    mc.block_until_active()
+    print(mc.status)
+    mc.pause()
+    time.sleep(5)
+    mc.play()
 
-print(cast.device, cast.status)
-
-mc = cast.media_controller
-mc.play_media(rns, 'audio/mpeg')
-mc.block_until_active()
-print(mc.status)
-mc.pause()
-time.sleep(5)
-mc.play()
+    # Shut down discovery
+    discovery.stop_discovery(browser)
 
 
-# Shut down discovery
-pychromecast.discovery.stop_discovery(browser)
+if __name__ == "__main__":
+    stations = dict(
+        nowyswiat='https://n10a-eu.rcs.revma.com/ypqt40u0x1zuv',
+        tokfm='https://radiostream.pl/tuba10-1.mp3',
+        baobab='http://stream.radiobaobab.pl:8000/radiobaobab.mp3'
+    )
+    station = stations['baobab']
+    play_stream(device_name='Pizza', url=station)
