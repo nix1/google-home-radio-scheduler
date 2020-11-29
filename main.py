6064ef157b702076b3d42f769825ae7643bb0b4b
Paweł Sierszeń <pawelsierszen@gmail.com>
2020-11-29 13:19:11 +0100

    Move station definitions to a yml file

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ from play import play_stream
 import schedule
 import time
 import yaml
+import validators
 
-STATIONS = dict(
-    nowyswiat='https://n10a-eu.rcs.revma.com/ypqt40u0x1zuv',
-    tokfm='https://radiostream.pl/tuba10-1.mp3',
-    baobab='http://stream.radiobaobab.pl:8000/radiobaobab.mp3'
-)
+with open('stations.yml') as stations_file:
+    STATIONS = yaml.safe_load(stations_file)
 
 
-def play_station(station_id, device_name):
-    station_url = STATIONS[station_id]
-    print(f"Starting the {station_id} radio station, playing on {device_name}...")
-    play_stream(device_name=device_name, url=station_url)
+def play_station(station, device_name):
+    print(f"Starting the {station['name']} radio station, playing on {device_name}...")
+    play_stream(device_name=device_name, url=station['url'])
 
 
-if __name__ == "__main__":
+def main():
     with open('schedule.yml') as schedule_file:
         schedule_config = yaml.safe_load(schedule_file)
 
     for day, jobs in schedule_config.items():
         for job in jobs:
-            station = job['station']
+            station = STATIONS[job['station']]
             at = job['at']
             device = job['device']
-            print(f"{day}: scheduling {station} for {at} on {device}")
+            print(f"{day.capitalize()}: scheduling {station['name']} for {at} on {device}")
+            assert validators.url(station['url'])
             scheduler = getattr(schedule.every(), day)
             try:
                 scheduler.at(at).do(play_station, station, device)
@@ -36,3 +34,7 @@ if __name__ == "__main__":
     while True:
         schedule.run_pending()
         time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
